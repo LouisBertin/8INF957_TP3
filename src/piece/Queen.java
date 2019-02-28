760c8dfa660ada2d38d8b39c8771ab1211a35529
LouisBertin <bertin.louis7@gmail.com>
2019-02-28 16:10:55 -0500

    first commit :tada:

---
+++ src/piece/Queen.java
@@ -0,0 +1,24 @@
+package piece;
+
+import agent.Move;
+import agent.Player;
+
+public class Queen extends Piece {
+
+    public Queen(int player) {
+        super(player);
+    }
+
+    @Override
+    public String toString() {
+        return ((this.player == Player.WHITE) ? "D" : "d");
+    }
+
+    @Override
+    public boolean isMoveLegal(Move mv) {
+        // TODO Auto-generated method stub
+        Rook r = new Rook();
+        Bishop b = new Bishop();
+        return r.isMoveLegal(mv) || b.isMoveLegal(mv);
+    }
+}
